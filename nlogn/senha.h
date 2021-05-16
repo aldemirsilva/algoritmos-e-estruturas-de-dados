4ea469f0e583a45392dc4e79b48cc9e4af1928ba
aldemirsilva <aldemir.rodrigues@gmail.com>
2021-05-16 16:44:01 -0400

    Delete nlogn directory

--- nlogn/senha.h
+++
@@ -1,5 +0,0 @@
-typedef struct senha TSenha;
-TSenha *criarSenha(int num, int hora, int min);
-void imprimirSenha(TSenha *senha);
-void destroySenha(TSenha *s);
-int compararSenha(void *s1, void *s2);
