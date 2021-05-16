8af91587373f8b03e6dd9410ad9a31aa661d12b4
aldemirsilva <aldemir.rodrigues@gmail.com>
2021-05-16 16:49:22 -0400

    Delete 2.2.1. Pilha, Fila, Fila de Prioridade com ListasArquivo directory

--- 2.2.1. Pilha, Fila, Fila de Prioridade com ListasArquivo/fila.h
+++
@@ -1,9 +0,0 @@
-typedef struct fila{
-  TLista *str;
-  // estatisticas da fila
-}TFila;
-
-TFila *criarFila();
-void mostrarFila(TFila *f);
-void enfileirarFila(TFila *f, void *carga);
-void *desenfileirarFila(TFila *f);
--- 2.2.1. Pilha, Fila, Fila de Prioridade com ListasArquivo/pilha.h
+++
@@ -1,8 +0,0 @@
-typedef struct pilha TPilha;
-
-TPilha *criarPilha();
-void empilharPilha(TPilha *p, void *novo);
-void *desempilharPilha(TPilha *p);
-void *topoPilha(TPilha *p);
-int tavaziaPilha(TPilha *p);
-int medirPilha(TPilha *p);
